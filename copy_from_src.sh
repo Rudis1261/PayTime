fb8a66d02f962af5bce8f83be9468f85e146385f
Rudi Strydom <iam@thatguy.co.za>
2015-07-26 22:12:20 +0200

    Bootstrap

---
+++ copy_from_src.sh
@@ -0,0 +1,3 @@
+#!/bin/sh
+cp ~/DockIt/src/public/serge/assets/js/payment/payTime.js .
+cp ~/DockIt/src/public/serge/assets/css/payment/payTime.css .
---
+++ payTime.js
@@ -0,0 +1,30 @@
+// Time to privatize
+(function ($) {
+
+    // The data-module selector
+    var dataInit = $('[data-module="paytime"]');
+
+    // Main function
+    $.fn.payTime = function() {
+        var items = $('.item', this);
+        var navs = $('.nav', this);
+
+        // Check for items
+        if (typeof items === "undefined" || typeof items.length === 0) {
+            console.log("No items to work against");
+            return this;
+        }
+
+        // OnClick toggle the class to the item you would like to be active
+        items.on('click', function(){
+            items.removeClass('active');
+            $(this).addClass('active');
+            return this;
+        };
+
+    // Auto init via data
+    if (dataInit.length > 0) {
+        dataInit.payTime();
+    }
+
+}( jQuery ));
